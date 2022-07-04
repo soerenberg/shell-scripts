89a1541b5efc61c53235516e7828594b724e33ef
Soeren Berg <soeren.berg@protonmail.com>
2022-07-04 10:06:31 +0200

    Remove directories in select_example.sh

--- select_example.sh
+++ select_example.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-items=$(ls)
-PS3="cat which file? "  # select prompt
+raw_items=$(ls)
 
+# We filter out directories. We could have used an appropriate shell command
+# above (e.g., ls with grep) to do this, but we use a for loop and the `-d`
+# condition for demonstration purposes.
+for file in $raw_items; do
+    if [ ! -d $file ]; then
+        items="$items $file"
+    fi
+done
+
+
+PS3="cat which file? "  # select prompt
 select selection in $items; do
     if [ $selection ]; then
         echo selected $selection with number $REPLY
