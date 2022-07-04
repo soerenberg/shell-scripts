1266d2894f14aad0650fe68d6a2268d3cd06e11f
Soeren Berg <soeren.berg@protonmail.com>
2022-07-04 10:36:45 +0200

    Add case example to select_example.sh

--- select_example.sh
+++ select_example.sh
@@ -12,13 +12,40 @@ for file in $raw_items; do
 done
 
 
-PS3="cat which file? "  # select prompt
+PS3="Select file? "  # select prompt
 select selection in $items; do
     if [ $selection ]; then
         echo selected $selection with number $REPLY
-        echo "$(cat $selection)"  # use quotes to print newlines
+        selected_file=$selection
         break
     else
         echo "invalid try again"
     fi
 done
+
+
+options="cat file,print write permission,print execute permissions"
+IFS_OLD=$IFS  # Since the actions contain spaces we temporarily change IFS.
+IFS=","
+PS3="Select action "
+select selection in $options; do
+    if [ $selection ]; then
+        case $REPLY in
+            "1") echo "$(cat $selected_file)" ;;  # use quotes to print newlines
+            "2") if [ -w $selected_file ]; then
+                    echo "You have write permission"
+                 else
+                    echo "You do not have write permissions."
+                 fi ;;
+            "3") if [ -x $selected_file ]; then
+                    echo "You have execute permission"
+                 else
+                    echo "You do not have execute permissions."
+                 fi ;;
+        esac
+        break
+    else
+        echo "invalid try again"
+    fi
+done
+IFS=$IFS_OLD
